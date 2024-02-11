2c10eede218036e2233a1b4e3d310efbbee30965
Your Name <you@example.com>
2024-02-11 23:16:16 +0500

    Task 7

--- src/main/java/ru/gb/springdemo/model/Reader.java
+++ src/main/java/ru/gb/springdemo/model/Reader.java
@@ -6,13 +6,7 @@ import jakarta.persistence.*;
 
 @Data
 public class Reader {
-
   public static long sequence = 1L;
   private final long id;
   private final String name;
-
-//  public Reader(String name) {
-//    this(sequence++, name);
-//  }
-
 }
