391d493ddd13210f205ab2e8a72aed07b0ebc480
Your Name <you@example.com>
2024-02-22 20:17:06 +0500

    task10

--- src/main/java/ru/gb/springdemo/api/BookRequest.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.springdemo.api;
-
-import lombok.Data;
-
-@Data
-public class BookRequest {
-    private String name;
-}
--- src/main/java/ru/gb/springdemo/api/ReaderRequest.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.springdemo.api;
-
-import lombok.Data;
-
-@Data
-public class ReaderRequest {
-    private String name;
-}
